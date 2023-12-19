1faf51ac486d53277e0bc8908bc50e2b7d3ebd85
chris ho <98445729+ho0527@users.noreply.github.com>
2023-12-19 20:49:51 +0800

    new: 可以上傳 郵件了:)

--- connect.js
+++ connect.js
@@ -1,3 +1,5 @@
+let isupload=false
+
 docgetid("submit").onclick=function(){
     let errorlist=[]
 
@@ -14,21 +16,28 @@ docgetid("submit").onclick=function(){
     if(docgetid("email").value==""){
         errorlist.push("\"電子信箱\"")
     }
-    if(docgetid("description").value==""){
+    if(docgetid("content").value==""){
         errorlist.push("\"需求說明\"")
     }
     // 驗證資料 END
 
     if(errorlist.length==0){
-        alert("功能尚未製作")
-        location.reload()
-        ajax("POST","/backend/project005/newconnect",function(event){
-            let data=JSON.parse(event.responseText)
-            if(data["success"]){
-                alert("上傳成功")
-                location.reload()
-            }
-        })
+        if(!isupload){
+            isupload=true
+            ajax("POST","/backend/project005/newconnect",function(event){
+                let data=JSON.parse(event.responseText)
+                if(data["success"]){
+                    alert("上傳成功")
+                    location.reload()
+                }
+            },JSON.stringify({
+                "companyname": docgetid("companyname").value,
+                "name": docgetid("name").value,
+                "phone": docgetid("phone").value,
+                "email": docgetid("email").value,
+                "content": docgetid("content").innerHTML
+            }))
+        }
     }else{
         docgetid("error").innerHTML=`
             ${errorlist.join("、")}為必填欄位，請填寫完畢。
